c70ccba91c8fa6a0ebab13f76d6121c81fbb8938
Tomáš Kalabis <Tomas Kalabis>
2022-10-06 16:07:39 +0200

    added WIP

--- vector.h
+++ vector.h
@@ -8,31 +8,104 @@ namespace epc
    template <typename T>
    class vector
    {
-      public:
-         vector() noexcept { }
+   private:
+       size_t t_capacity; // allocated space
+       size_t t_size; // number of elements in allocated space
+       T* t_data;
+   public:
+         vector() noexcept {
+             t_capacity = 0;
+             t_data = nullptr;
+             t_size = 0;
+         }
 
-         vector(const vector&) { }
+         vector(const vector& v) {//: t_capacity(v.t_capacity), t_size(v.t_size){
+             T* tmp = new T[t_capacity];
+             delete [] t_data;
+             t_data = tmp;
 
-         vector& operator=(const vector&) { }
+             t_capacity = v.t_capacity;
+//                t_data = operator new (t_capacity * sizeof(T));
+             t_size = v.t_size;
+             for (int i = 0; i < v.t_size; i ++) {
+                 t_data[i] = v[i];
+             }
+         }
 
-         ~vector() { }
+         vector& operator=(const vector& v) {
+             if (&v == this)
+                 return *this;
 
-         T* data() { }
-         const T* data() const { }
+             delete [] t_data;
 
-         T& operator[](size_t) { }
-         const T& operator[](size_t) const { }
+             t_capacity = v.t_capacity;
+             t_data = new T[t_capacity];
+             t_size = v.t_size;
+             for (int i = 0; i < v.t_size; i ++) {
+                 t_data[i] = v[i];
+             }
 
-         void push_back(const T&) { }
+             return *this;
+         }
 
-         void swap(vector& other) noexcept { }
+         ~vector() {
+             delete [] t_data;
+         }
 
-         size_t capacity() const { }
-         size_t size() const { }
+         T* data() {
+             return t_data;
+         }
+         const T* data() const {
+             return t_data;
+         }
 
-         void reserve(size_t) { }
+         T& operator[](size_t i) {
+             return t_data[i];
+         }
+         const T& operator[](size_t i) const {
+             return t_data[i];
+         }
 
-         void pop_back() { } 
+         void push_back(const T& t) {
+             if (t_size == t_capacity) {
+                 t_capacity = t_capacity == 0 ? 1 : t_capacity * 2;
+                 T* tmp = new T[t_capacity];
+                 for (size_t i = 0; i < t_size; i ++)
+                     tmp[i] = t_data[i];
+                 delete [] t_data;
+                 t_data = tmp;
+             }
+             t_data[t_size ++] = t;
+         }
+
+         void swap(vector& other) noexcept {
+            vector tmp = vector(other);
+            other = *this;
+            *this = tmp;
+         }
+
+         size_t capacity() const {
+             return t_capacity;
+         }
+         size_t size() const {
+             return t_size;
+         }
+
+         void reserve(size_t s) {
+             if (s > t_size) {
+                 T * tmp = new T[s];
+                 for (size_t i = 0; i < t_size; i ++)
+                     tmp[i] = t_data[i];
+                 delete [] t_data;
+                 t_data = tmp;
+                 t_capacity = s;
+             }
+         }
+
+         void pop_back() {
+            if (t_size > 0)
+                t_size --;
+         }
    };
 }
 
