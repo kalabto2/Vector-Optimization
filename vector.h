b1e11eec500438b4309cc4d9d494a6156dda8cf0
Tomáš Kalabis <kalabto2@fit.cvut.cz>
2022-12-03 20:17:14 +0100

    fix memleaks

--- vector.h
+++ vector.h
@@ -12,12 +12,12 @@ namespace epc
            size_t t_capacity; // allocated space
            size_t t_size; // number of elements in allocated space
            T *t_data; // array
-           alignas(T) std::byte t_buffer[sizeof(T) * N]; // static buffer
+           std::byte t_buffer[sizeof(T) * N]; // static buffer
 
       public:
        vector() noexcept {
            t_capacity = N;
-            t_data = reinterpret_cast<T*>(t_buffer);
+           t_data = reinterpret_cast<T*>(t_buffer);
            t_size = 0;
        }
 
@@ -25,7 +25,8 @@ namespace epc
            T *tmp;
            if (v.t_capacity <= N)
                tmp = reinterpret_cast<T*>(t_buffer);
-           tmp = static_cast<T *>(::operator new(sizeof(T) * v.t_capacity));
+           else
+                tmp = static_cast<T *>(::operator new[](sizeof(T) * v.t_capacity));
 
            int i = 0;
            try {
@@ -64,7 +65,7 @@ namespace epc
            v.t_size = 0;
            v.t_capacity = N;
            v.t_data = nullptr;
-       } // todo move source vector to target vector
+       }
 
        vector& operator=(vector&& v) {
            for ( int i = 0; i < t_size; i++ )
@@ -83,11 +84,10 @@ namespace epc
                }
            }
 
-
            v.t_size = 0;
            v.t_capacity = N;
            v.t_data = nullptr;
-       } // todo move source vector to target vector
+       }
 
        ~vector() {
            for ( int i = 0; i < t_size; i++ )
@@ -121,18 +121,14 @@ namespace epc
            return t_size;
        }
 
-       void reserve(size_t s) { // todo move
+       void reserve(size_t s) {
            if (s > t_size) {
-               T *tmp = static_cast<T *>(::operator new(sizeof(T) * s));
+               T *tmp = static_cast<T *>(::operator new[](sizeof(T) * s));
                size_t i = 0;
 
                try {
                    for ( ; i < t_size; i++ )
-//                        new(tmp + i) T(std::forward<T>(t_data[i]));
-//                       tmp[i] = std::move(t_data[i]);
-//                       new(tmp + i) T(t_data[i]);
-                       new(tmp + i) T(std::move(t_data[i])); // todo what to do here?
-//                        new(tmp + i) T(static_cast<T&&>(t_data[i]));
+                       new(tmp + i) T(std::move(t_data[i]));
                } catch (...) {
                    for ( int j = 0; j < i; j++ )
                        tmp[j].T::~T();
@@ -144,7 +140,6 @@ namespace epc
                for ( int j = 0; j < t_size; j++ )
                    t_data[j].T::~T();
 
-//               operator delete[](t_data);
                 // free memory only from dynamic allocation
                if (t_size > N)
                     operator delete[](t_data);
@@ -154,101 +149,33 @@ namespace epc
            }
        }
 
-       void push_back(const T &t) { // todo really done? I didn't do anything
-           if (t_size == t_capacity) { // reallocate array
+       void push_back(const T &t) {
+           if (t_size == t_capacity) // reallocate array
                reserve((t_capacity == 0 ? 1 : t_capacity * 2));
-//               T *tmp = static_cast<T *>(::operator new(sizeof(T) * (t_capacity == 0 ? 1 : t_capacity * 2)));
-//               size_t i = 0;
-//
-//               try {
-//                   // copy new elements
-//                   for ( ; i < t_size; i++ )
-//                       new(tmp + i)T(t_data[i]);
-//               } catch (...) {
-//                   for ( size_t j = 0; j < i; j++ )
-//                       tmp[i].T::~T();
-//
-//                   operator delete[](tmp);
-//                   throw;
-//               }
-//
-//               // destroy all elements (buffer + dynamic allocation)
-//               for ( int j = 0; j < t_size; j++ )
-//                   t_data[j].T::~T();
-//
-//               // free memory only from dynamic allocation
-//               if (t_size > N)
-//                    operator delete[](t_data);
-//
-//               t_data = tmp;
-//               t_capacity = t_capacity == 0 ? 1 : t_capacity * 2;
-           }
 
+           // copy lvalue element
            new(t_data + t_size) T(t);
            t_size++;
        }
 
        void push_back(T&& t) {
-           if (t_size == t_capacity) { // reallocate array
+           if (t_size == t_capacity) // reallocate array
                reserve((t_capacity == 0 ? 1 : t_capacity * 2));
-//               T *tmp = static_cast<T *>(::operator new(sizeof(T) * (t_capacity == 0 ? 1 : t_capacity * 2)));
-//               size_t i = 0;
-//
-//               try {
-//                   for ( ; i < t_size; i++ )
-//                       new(tmp + i)T(t_data[i]);
-//               } catch (...) {
-//                   for ( size_t j = 0; j < i; j++ )
-//                       tmp[i].T::~T();
-//
-//                   operator delete[](tmp);
-//                   throw;
-//               }
-//
-//               for ( int j = 0; j < t_size; j++ )
-//                   t_data[j].T::~T();
-//               if (t_size > N)
-//                   operator delete[](t_data);
-//
-//               t_data = tmp;
-//               t_capacity = t_capacity == 0 ? 1 : t_capacity * 2;
-           }
 
+           // move rvalue to this vector
            new(t_data + t_size) T(std::move(t));
            t_size++;
-       } // todo really done??
+       }
 
        template <typename... Ts>
          void emplace_back(Ts&&... ts) {
-           if (t_size == t_capacity) { // reallocate array
+           if (t_size == t_capacity) // reallocate array
                reserve((t_capacity == 0 ? 1 : t_capacity * 2));
-//               T *tmp = static_cast<T *>(::operator new(sizeof(T) * (t_capacity == 0 ? 1 : t_capacity * 2)));
-//               size_t i = 0;
-//
-//               try {
-//                   for ( ; i < t_size; i++ )
-//                       new(tmp + i)T(t_data[i]);
-//               } catch (...) {
-//                   for ( size_t j = 0; j < i; j++ )
-//                       tmp[i].T::~T();
-//
-//                   operator delete[](tmp);
-//                   throw;
-//               }
-//
-//               for ( int j = 0; j < t_size; j++ )
-//                   t_data[j].T::~T();
-//               if (t_size > N)
-//                   operator delete[](t_data);
-//
-//               t_data = tmp;
-//               t_capacity = t_capacity == 0 ? 1 : t_capacity * 2;
-           }
 
            // perfect forwarding used for initialization
            new (t_data + t_size) T(std::forward<Ts>(ts)...);
            t_size ++;
-       } // todo
+       }
 
        void pop_back() {
            t_data[t_size - 1].T::~T();
